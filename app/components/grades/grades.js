f6ecc6c98b9cb97d359675ddd2655b70116bfb9d
thanhdn <dngthanh@hotmail.com>
2017-10-17 21:17:50 +0700

    add two features: update grade and delete grade

--- app/components/grades/grades.js
+++ app/components/grades/grades.js
@@ -2,56 +2,86 @@
 
 angular.module('schoolCalendarApp.grades', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/add-new-grade', {
-    templateUrl: 'components/grades/add.html',
-    controller: 'GradesController'
-  }).when('/grades', {
-      templateUrl: 'components/grades/grades.html',
-      controller: 'GradesController'
-  }).when('/detail', {
-      templateUrl: 'components/grades/detail.html',
-      controller: 'GradesController'
+    .config(['$routeProvider', function($routeProvider) {
+        $routeProvider.when('/add-new-grade', {
+            templateUrl: 'components/grades/add.html',
+            controller: 'GradesController'
+        }).when('/grades', {
+            templateUrl: 'components/grades/grades.html',
+            controller: 'GradesController'
+        }).when('/detail', {
+            templateUrl: 'components/grades/detail.html',
+            controller: 'GradesController'
 
-  });
-}])
-
-.controller('GradesController', ['$scope', 'repository', '$location', function($scope, repository, $location) {
-
-    $scope.result = "";
-    $scope.lstGrades = "";
-
-    $scope.initDisplayListGrades = function() {
-        repository.retrieve("http://localhost:8080/grade/view", {})
-            .success(function(data) {
-                $scope.lstGrades = data;
-            });
-    };
-
-    $scope.initDisplayListGrades();
-
-    $scope.inputGrade = {
-        id:"",
-        gradeId: "",
-        gradeName: "",
-        lectureNumber: "",
-        creationDate : "",
-        updateDate : "",
-        creator : ""
-    };
-    $scope.gradeId = "";
-    $scope.gradeName = "";
-    $scope.lectureNumber = "";
-    $scope.saveGrade = function() {
-        repository.add("http://localhost:8080/grade/add", $scope.inputGrade)
-        .success(function(data) {
-            $scope.result = data;
-            $scope.inputGrade = {};
-            $scope.initDisplayListGrades();
-        }).error(function (err) {
-            $scope.result = err;
         });
-    }
-}]);
+    }])
+
+    .controller('GradesController',
+            ['$scope',
+            'repositoryServices',   // the central services
+            'collectionUtil',       // the common functions for work with array
+            'componentObj',         // the compoment objects used for binding values
+            '$location',
+            '$window',
+            function($scope, repositoryServices, collectionUtil, componentObj, $location, $window) {
+
+                // the parameter holding any value binding from the edit form
+                $scope.inputGrade = componentObj.inputGrade;
+
+                // the parameter holding the keywords, pagination information
+                $scope.inputFilterGrade = componentObj.inputFilterGrade;
+
+                // the list grade
+                $scope.lstGrades = [];
+
+                $scope.retrieveGrades = function() {
+                    repositoryServices.retrieve("http://localhost:8080/grade/view", $scope.inputFilterGrade)
+                        .success(function(data) {
+                            $scope.lstGrades = data;
+                        }).error(function(err) {
+
+                    });
+                };
+
+                // displays the list grades after the page has fetched
+                $scope.retrieveGrades();
+
+                $scope.saveGrade = function() {
+                    repositoryServices.add("http://localhost:8080/grade/add", $scope.inputGrade)
+                        .success(function(data) {
+                            // reset the form
+                            $scope.inputGrade = {action:"INS"};
+                            // refresh the list grades after insert or update a record
+                            $scope.retrieveGrades();
+                        }).error(function (err) {
+
+                    });
+                };
+
+
+                // init data on update event
+                $scope.editGrade = function(_grade) {
+                    // check whether the target update grade is existed,
+                    // to prevent the conflict between UI data and DB data
+                    if(collectionUtil.isExistedPropertyValue("id", _grade, $scope.lstGrades)) {
+                        $scope.inputGrade["grade"] = angular.copy(_grade);
+                    }
+                    $scope.inputGrade["action"] = repositoryServices.updateAction;
+                    // scroll to the update form position
+                    $window.scrollTo(0, 0);
+                };
+
+                $scope.removeGrade = function(_grade) {
+                    // check whether the target update grade is existed,
+                    // to prevent the conflict between UI data and DB data
+                    if(collectionUtil.isExistedPropertyValue("id", _grade, $scope.lstGrades)) {
+                        $scope.inputGrade["grade"] = angular.copy(_grade);
+                    }
+                    // setting the action to the parameters
+                    $scope.inputGrade["action"] = repositoryServices.removeAction;
+                    // calling service to remove the target grade
+                    $scope.saveGrade();
+                }
+            }]);
 
 
--- app/shared/repository.js
+++ app/shared/repository.js
@@ -1,8 +1,14 @@
 
-schoolCalendarApp.
-factory('repository', ['$http', function($http) {
+'use strict';
+
+schoolCalendarApp.factory('repositoryServices', ['$http', function($http) {
     return {
-        retrieve: function(URL, parameters) {
+        // update action, used to dertermine what action do the user need to perform
+        updateAction : 'UDP',
+        // remove action, used to dertermine what action do the user need to perform
+        removeAction : 'REM',
+
+        retrieve : function(URL, parameters) {
             return $http.get(URL, parameters)
                 .success(function (data) {
                     return data;
@@ -12,7 +18,7 @@ factory('repository', ['$http', function($http) {
                 });
         },
 
-        add: function(URL, parameters) {
+        add : function(URL, parameters) {
             return $http.post(URL, parameters)
                 .success(function (data) {
                     return data;
@@ -20,6 +26,96 @@ factory('repository', ['$http', function($http) {
                 .error(function (err) {
                     return err;
                 });
+        },
+
+        remove : function(URL, parameters) {
+            return $http.post(URL, parameters)
+                .success(function (data) {
+                    return data;
+                })
+                .error(function (err) {
+                    return err;
+                });
+        }
+    }
+}]).factory('collectionUtil', [function() {
+    return {
+        isEmptyArray : function(collection) {
+            if(collection == null || collection.length == 0) {
+                return true;
+            }
+            return false;
+        },
+
+        isExistedPropertyValue : function(propertyName, dest, collection) {
+            if(typeof collection === 'undefined' ||
+                typeof dest === 'undefined') {
+                return false;
+            }
+
+            for(var idx = 0; idx < collection.length; idx++) {
+                if(collection[idx][propertyName] == dest[propertyName]) {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+
+    }
+}]).factory('componentObj', [function() {
+    return {
+        // --GRADE
+        //--------------------------------------------------------------
+        // the parameter holding any value binding from the edit form
+        inputGrade :  {
+            grade: {
+                id            :"",
+                gradeId       : "",
+                gradeName     : "",
+                lectureNumber : "",
+                creationDate  : "",
+                updateDate    : "",
+                creator       : ""
+            },
+            // UDP: update, REM: remove
+            action        : "INS"
+        },
+
+        // the parameter holding the keywords, pagination information
+        inputFilterGrade : {
+            keywords       : [],
+            startDate      : "",
+            endDate        : "",
+            page           : 0,
+            fetchingNumber : 50
+    }
+}
+}]).factory('repositoryInterface', ['repositoryServices', function(repositoryServices) {
+    return {
+        CONTEXT : 'http://localhost:8080',
+
+        targetFeature: "",
+
+        formInput:  {},
+
+        formFilter: {},
+
+        save : function() {
+
+        },
+
+        edit : function() {
+
+        },
+
+        remove : function() {
+
+        },
+
+        filter : function() {
+
         }
     }
 }]);
